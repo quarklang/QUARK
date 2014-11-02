efb48c5e8804b67967642d0953bcceee314cd0c3
Jamis Johnson <jamismanwaring@gmail.com>
2014-11-02 17:57:23 -0500

    fixes bootstrap.sh by running apt-get update after adding repo avsm
    previous error was an inability to find package opam

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y python-software-properties
-add-apt-repository ppa:avsm/ppa
-apt-get update
-apt-get install -y ocaml opam
+sudo apt-get -y update
+apt-get install -y python-software-properties
+add-apt-repository ppa:avsm/ppa && apt-get update
+apt-get install -y ocaml ocaml-native-compilers camlp4-extra opam
 rm -rf /var/www
 ln -fs /vagrant /var/www
