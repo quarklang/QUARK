d51c72cf930caa4befbb8fe995ec145dbbc9acb1
JimJarvis <jimfanspire@gmail.com>
2014-12-07 16:31:45 -0500

    Add op_tag datatype to tag the Binop SAST better

--- quark/sast.ml
+++ quark/sast.ml
@@ -1,13 +1,20 @@
 module A = Ast
 module T = Type
 
+(* tag what operator is actually used in C++ *)
+type op_tag = 
+  | OpVerbatim  (* no change to the operator *)
+  | CastComplex1 (* cast the first arg to complex *)
+  | CastComplex2 (* cast the second arg to complex *)
+  | OpConcat (* list concatenation *)
+
 type lvalue =
   | Variable of A.datatype * A.ident
   | ArrayElem of A.datatype * A.ident * expr list
 (* TODO? add MatrixElem of A.ident * expr * A.datatype *)
 
 and expr =
-  | Binop of A.datatype * expr * A.binop * A.datatype * expr
+  | Binop of expr * A.binop * expr * op_tag
   | AssignOp of A.datatype * lvalue * A.binop * A.datatype * expr
   | Unop of A.unop * A.datatype * expr
   | PostOp of A.datatype * lvalue * A.postop
@@ -24,7 +31,6 @@ and expr =
   | Membership of A.datatype * expr * A.datatype * expr
   | FunctionCall of A.ident * expr list
   | Lval of lvalue
-  
 
 type decl =
   | AssigningDecl of A.datatype * A.ident * expr
