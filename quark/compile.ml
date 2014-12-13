44b224f535f978980f59051ed2185b2189eb2eab
JimJarvis <jimfanspire@gmail.com>
2014-12-13 01:04:14 -0500

    Fully support import and elif syntax. Handle circular/repeated import correctly

--- quark/compile.ml
+++ quark/compile.ml
@@ -1,24 +1,16 @@
 open Semantic
 
-let read_file filename = 
-  let lines = ref [] in
-  let chan = open_in filename in
-  try
-    while true; do
-      lines := input_line chan :: !lines
-    done; []
-  with End_of_file ->
-    close_in chan;
-    List.rev !lines
-
 let _ =
 
+  print_endline @@ Preprocessor.process "../what.qk"
+  (*
   let lexbuf = Lexing.from_string "elif shit; import     \t\t  asdf  \"import\"; import elif asdf; elif import shit     ; " in
   let processed_code, imports = Preprocessor.process "" [] lexbuf in
   let importss = List.fold_left (fun acc x -> acc ^x^ ":: ") "" imports in
-  print_endline @@ processed_code ^"\nimported("^string_of_int (List.length imports)^ "): "^ importss
+  let _ = 
+    print_endline @@ processed_code ^"\nimported("^string_of_int (List.length imports)^ "): "^ importss in
+  print_endline @@ Preprocessor.read_file_str "../what.qk"
   
-  (*
   let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.top_level Scanner.token lexbuf in
   let env = { 
