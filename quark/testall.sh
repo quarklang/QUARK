82eae50f020fc538d0997c2754535e80bb7fce98
Daria Jung <djj2115cu@gmail.com>
2014-12-14 16:22:16 -0500

    add gcd

--- quark/testall.sh
+++ quark/testall.sh
@@ -5,13 +5,13 @@ COMPILER="./quarkc"
 for TESTFILE in tests/*.qk;
 do
     echo "  TESTING $TESTFILE"
+    
     LEN=$((${#TESTFILE}-3))
     CPPOUTNAME="${TESTFILE:0:$LEN}.cpp"
-    echo "$CPPOUTNAME"
     EXECUTABLENAME="${TESTFILE:0:$LEN}"
     OUTFILENAME="${TESTFILE:0:$LEN}.coutput"
-    echo "$OUTFILENAME"
     TESTFILENAME="${TESTFILE:0:$LEN}.output"
+
     "$COMPILER" -s "$TESTFILE" -c "$CPPOUTNAME" -o "$EXECUTABLENAME"
     ./"$EXECUTABLENAME" > "$OUTFILENAME"
     if (diff "$OUTFILENAME" "$TESTFILENAME") 
