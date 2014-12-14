76b1e92de05f37fc52f05dba8169179e7b8dee35
Daria Jung <djj2115cu@gmail.com>
2014-12-13 22:41:57 -0500

    get testing script working

--- quark/testall.sh
+++ quark/testall.sh
@@ -1,119 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+#script used for reg testing
+COMPILER="./quarkc"
 
-QUARKC="./quarkc"
-
-# Set time limit for all operations
-ulimit -t 30
-
-globallog=testall.log
-rm -f $globallog
-error=0
-globalerror=0
-
-keep=0
-
-Usage() {
-    echo "Usage: testall.sh [options] [.qk files]"
-    echo "-k    Keep intermediate files"
-    echo "-h    Print this help"
-    exit 1
-}
-
-SignalError() {
-    if [ $error -eq 0 ] ; then
-	echo "FAILED"
-	error=1
-    fi
-    echo "  $1"
-}
-
-# Compare <outfile> <reffile> <difffile>
-# Compares the outfile with reffile.  Differences, if any, written to difffile
-Compare() {
-    generatedfiles="$generatedfiles $3"
-    echo diff -b $1 $2 ">" $3 1>&2
-    diff -b "$1" "$2" > "$3" 2>&1 || {
-	SignalError "$1 differs"
-	echo "FAILED $1 differs from $2" 1>&2
-    }
-}
-
-# Run <args>
-# Report the command, run it, and report any errors
-Run() {
-    echo $* 1>&2
-    eval $* || {
-	SignalError "$1 failed on $*"
-	return 1
-    }
-}
-
-Check() {
-    error=0
-    basename=`echo $1 | sed 's/.*\\///
-                             s/.qk//'`
-    reffile=`echo $1 | sed 's/.qk$//'`
-    basedir="`echo $1 | sed 's/\/[^\/]*$//'`/."
-
-    echo -n "$basename..."
-
-    echo 1>&2
-    echo "###### Testing $basename" 1>&2
-
-    generatedfiles=""
-
-    generatedfiles="$generatedfiles ${basename}.test.out" &&
-    Run "$QUARKC" "<" $1 ">" ${basename}.test.out &&
-    Compare ${basename}.test.out ${reffile}.output ${basename}.test.out.diff
-
-    # Report the status and clean up the generated files
-
-    if [ $error -eq 0 ] ; then
-        if [ $keep -eq 0 ] ; then
-            rm -f $generatedfiles
-        fi
-        echo "OK"
-        echo "###### SUCCESS" 1>&2
-    else
-        echo "###### FAILED" 1>&2
-        globalerror=$error
-    fi
-}
-
-while getopts kdpsh c; do
-    case $c in
-	k) # Keep intermediate files
-	    keep=1
-	    ;;
-	h) # Help
-	    Usage
-	    ;;
-    esac
-done
-
-shift `expr $OPTIND - 1`
-
-if [ $# -ge 1 ]
-then
-    files=$@
-else
-    files="tests/fail-*.qk tests/test-*.qk"
-fi
-
-for file in $files
+for TESTFILE in tests/*.qk;
 do
-    case $file in
-	*test-*)
-	    Check $file 2>> $globallog
-	    ;;
-	*fail-*)
-	    CheckFail $file 2>> $globallog
-	    ;;
-	*)
-	    echo "unknown file type $file"
-	    globalerror=1
-	    ;;
-    esac
+    echo "  TESTING $TESTFILE"
+    LEN=$((${#TESTFILE}-3))
+    CPPOUTNAME="${TESTFILE:0:$LEN}.cpp"
+    echo "$CPPOUTNAME"
+    EXECUTABLENAME="${TESTFILE:0:$LEN}"
+    OUTFILENAME="${TESTFILE:0:$LEN}.coutput"
+    echo "$OUTFILENAME"
+    TESTFILENAME="${TESTFILE:0:$LEN}.output"
+    "$COMPILER" -s "$TESTFILE" -c "$CPPOUTNAME" -o "$EXECUTABLENAME"
+    ./"$EXECUTABLENAME" > "$OUTFILENAME"
+    if (diff "$OUTFILENAME" "$TESTFILENAME") 
+    then
+        echo "      ----- SUCESS -----"
+    else
+        echo "      ------- FAIL -----"
+    fi
+    rm "$OUTFILENAME" "$CPPOUTNAME" "$EXECUTABLENAME"
 done
-
-exit $globalerror
