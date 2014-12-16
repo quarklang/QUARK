57c7ece0e9928b955423f96fa69f62298484b9bb
Jamis Johnson <jamismanwaring@gmail.com>
2014-12-15 19:18:00 -0500

    removes unecessary rm from vagrant bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,8 @@ apt-get install -y ocaml ocaml-native-compilers camlp4-extra opam
 apt-get install -y vim
 apt-get install gcc-4.8 g++-4.8 -y
 rm /usr/bin/gcc
-rm /usr/bin/g++
 ln -s /usr/bin/gcc-4.8 /usr/bin/gcc
 ln -s /usr/bin/g++-4.8 /usr/bin/g++
 rm -rf /var/www
 ln -fs /vagrant /var/www
-cd /vagrant/
+cd /vagrant
